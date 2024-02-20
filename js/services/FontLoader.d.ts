20b02c4f42ecb5fb579e2621d8930928a73fb09c
Martin Wolf <hello@martinwolf.org>
2024-02-20 08:16:14 +0100

    remove older things

--- js/services/FontLoader.d.ts
+++
@@ -1,8 +0,0 @@
-export default class FontLoader {
-	addGroup(className: string, list: Array<FontFaceObserver>): void;
-	start(): void;
-}
-
-export class FontFaceObserver {
-	constructor(family: string, descriptors: object);
-}
